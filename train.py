0f51d1017944bdbb370900dd729fcd3ce5be4b48
Andreas Neubauer <andreas.neubauer90@gmx.de>
2020-12-23 17:15:18 +0000

    Change back to distance vectors but only for coordinates

--- train.py
+++ train.py
@@ -53,9 +53,9 @@ def argparser_function():
   parser.add_argument('--dataset_name',type=str,default='preparation')
   parser.add_argument('--col_names', type=str, default='["frame", "obj", "x", "y"]')
   parser.add_argument('--max_epoch',type=int, default=20)
-  parser.add_argument('--batch_size',type=int,default=2)   
+  parser.add_argument('--batch_size',type=int,default=512)   
   parser.add_argument('--run_info', type=str, default=None)
-  parser.add_argument('--steps',type=int, default=None, default=5) 
+  parser.add_argument('--steps',type=int, default=5) 
 
   # default hyperparameters in case we do not use optuna
   parser.add_argument('--emb_size',type=int,default=32) 
@@ -137,7 +137,7 @@ def dataset_and_folder_prep(args):
 
   return train_dataset, val_dataset, test_dataset
 
-def means_and_stds(train_dataset):
+def means_and_stds(train_dataset, feature_count):
   input_means=[]
   input_stds=[]
   target_means=[]
@@ -147,13 +147,13 @@ def means_and_stds(train_dataset):
     ind=train_dataset[:]['dataset']==i
 
     # take feature_count "velocity" values    
-    input_src = train_dataset[:]['src'][ind, 1:, :]
-    input_trg = train_dataset[:]['trg'][ind, :, :]
+    input_src = train_dataset[:]['src'][ind, 1:, feature_count:feature_count*2]
+    input_trg = train_dataset[:]['trg'][ind, :, feature_count:feature_count*2]
 
     # calculate mean and std over the features
     input_src_mean = torch.cat((input_src, input_trg), 1).mean((0, 1))
     input_src_std = torch.cat((input_src, input_trg), 1).std((0, 1))
-
+    
     # safe mean and std values of this dataset
     input_means.append(input_src_mean)
     input_stds.append(input_src_std)
@@ -161,7 +161,7 @@ def means_and_stds(train_dataset):
     # safe only coordinate velocities mean and std
     target_means.append(input_src_mean[:2])
     target_stds.append(input_src_std[:2])
-  
+
   # calculate the mean and std of the features of all datasets
   input_mean=torch.stack(input_means).mean(0)
   input_std=torch.stack(input_stds).std(0)        
@@ -169,6 +169,7 @@ def means_and_stds(train_dataset):
   # calculate the mean and std of only the coordinate velocities of all datasets
   target_mean=torch.stack(target_means).mean(0)
   target_std=torch.stack(target_stds).std(0)
+
 
   return input_mean, input_std, target_mean, target_std
 
@@ -192,7 +193,7 @@ def objective(trial):
   #print(f'Steps: {args.steps}')
   train_dataset, val_dataset, test_dataset = dataset_and_folder_prep(args)
 
-  input_mean, input_std, target_mean, target_std = means_and_stds(train_dataset)
+  input_mean, input_std, target_mean, target_std = means_and_stds(train_dataset, feature_count)
   scipy.io.savemat(f'models/{args.name}/norm.mat',{'mean':input_mean.cpu().numpy(),'std':input_std.cpu().numpy()})
   
   device=torch.device("cuda")
@@ -204,10 +205,7 @@ def objective(trial):
   tr_dl = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=0)
   val_dl = torch.utils.data.DataLoader(val_dataset, batch_size=args.batch_size, shuffle=True, num_workers=0)
   test_dl = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False, num_workers=0)
-  
-  
-  
-  
+   
 
   if args.run_info is not None:        
     save_comment = f'{args.run_info}_{save_comment}'
@@ -221,10 +219,10 @@ def objective(trial):
   args.heads = 2**trial.suggest_int('heads', 1, 4)
   args.dropout = trial.suggest_float('dropout', 0.1, 0.9)
 
-  args.layers = 2
-  args.emb_size = 2**5
-  args.heads = 2**1
-  # args.dropout = 0.1
+  #args.layers = 2
+  #args.emb_size = 2**5
+  #args.heads = 2**1
+  #args.dropout = 0.1
 
   model=individual_TF.IndividualTF(feature_count, 3, 3, N=args.layers, d_model=args.emb_size,
                                    d_ff=2048, h=args.heads, dropout=args.dropout,mean=[0,0],std=[0,0]).to(device)                              
@@ -259,12 +257,8 @@ def objective(trial):
       optim.optimizer.zero_grad()
 
       # the input consists of all features
-      #inp_wo_mean = batch['src'][:,1:,:]
-      #print(f'inp_wo_mean: {inp_wo_mean[0][0]}')
-      #print(f'input_mean: {input_mean}')
-      #print(f'input_std: {input_std}')
-      inp=(batch['src'][:,:,:].to(device)-input_mean.to(device))/input_std.to(device)
-      target=(batch['trg'][:, :, 0:2].to(device)-target_mean.to(device))/target_std.to(device)
+      inp=(batch['src'][:,1:,feature_count:feature_count*2].to(device)-input_mean.to(device))/input_std.to(device)
+      target=(batch['trg'][:, :-1, feature_count:feature_count+2].to(device)-target_mean.to(device))/target_std.to(device)
       target_c=torch.zeros((target.shape[0],target.shape[1],1)).to(device)
       target=torch.cat((target,target_c),-1)      
       start_of_seq = torch.Tensor([0, 0, 1]).unsqueeze(0).unsqueeze(1).repeat(target.shape[0],1,1).to(device)
@@ -276,11 +270,9 @@ def objective(trial):
 
       pred=model(inp, dec_inp, src_att, trg_att)
 
-      #print(f'inp: {inp[0][0]}')
-
       y_pred = pred[:, :,0:2].contiguous().view(-1, 2)
 
-      y_real = ((batch['trg'][:, :, 0:2].to(device)-target_mean.to(device))/target_std.to(device)).contiguous().view(-1, 2).to(device)
+      y_real = ((batch['trg'][:, :, feature_count:feature_count+2].to(device)-target_mean.to(device))/target_std.to(device)).contiguous().view(-1, 2).to(device)
                 
       loss = F.pairwise_distance(y_pred, y_real).mean() + torch.mean(torch.abs(pred[:,:,2]))
 
@@ -293,12 +285,12 @@ def objective(trial):
     epoch+=1
     log.add_scalar(f'Loss/train/', epoch_loss / len(tr_dl), epoch)
 
+
     with torch.no_grad():
       model.eval()
 
       val_loss=0
       step=0
-      model.eval()
       gt = []
       pr = []
       inp_ = []
@@ -313,7 +305,7 @@ def objective(trial):
         objs.append(batch['objs'])
         dt.append(batch['dataset'])
 
-        inp = (batch['src'][:, 1:, :].to(device) - input_mean.to(device)) / input_std.to(device)
+        inp = (batch['src'][:, 1:, feature_count:feature_count*2].to(device) - input_mean.to(device)) / input_std.to(device)
         src_att = torch.ones((inp.shape[0], 1, inp.shape[1])).to(device)
         start_of_seq = torch.Tensor([0, 0, 1]).unsqueeze(0).unsqueeze(1).repeat(inp.shape[0], 1, 1).to(device)
         dec_inp = start_of_seq
@@ -323,10 +315,8 @@ def objective(trial):
           out = model(inp, dec_inp, src_att, trg_att)
           dec_inp = torch.cat((dec_inp, out[:, -1:, :]), 1)
 
-            
         preds_tr_b = (dec_inp[:, 1:, 0:2] * target_std.to(device) + target_mean.to(device)).cpu().numpy().cumsum(1) + batch['src'][:, -1:, 0:2].cpu().numpy()
         pr.append(preds_tr_b)
-        print("val epoch %03i/%03i  batch %04i / %04i" % (epoch, args.max_epoch, id_b, len(val_dl)))
 
 
       objs = np.concatenate(objs, 0)
@@ -338,32 +328,21 @@ def objective(trial):
       mad, fad, errs = baselineUtils.distance_metrics(gt, pr)
       log.add_scalar('validation/MAD', mad, epoch)
       log.add_scalar('validation/FAD', fad, epoch)
+    
+      if epoch==1:
+        torch.save(model.state_dict(),f'models/{args.name}/{epoch:05d}.pth')
 
-    trial.report(mad, epoch)
-
-    # Handle pruning based on the intermediate value.
-    if trial.should_prune():
-      raise optuna.TrialPruned()
-
-    if epoch==1:
-      torch.save(model.state_dict(),f'models/{args.name}/{epoch:05d}.pth')
-
-    if epoch%args.print_step==0:         
-      print("Epoch: %03i/%03i  Training time: %03.4f  Loss: %03.4f  Avg. Loss: %03.4f" % (epoch, args.max_epoch, time.time()-e_t0, epoch_loss, epoch_loss / len(tr_dl)))
+      if epoch%args.print_step==0:         
+        print(f"Epoch: {epoch:03d}/{args.max_epoch:03d}  Training time: {time.time()-e_t0:03.4f}  Loss: {epoch_loss:03.4f}  Avg. Loss: {epoch_loss / len(tr_dl):03.4f} MAD: {mad:03.4f} FAD: {fad:03.4f}") 
 
   print("Total training time: %07.4f" % (time.time()-t0))
 
-
-  
   return mad
-
-
-
 
 
 if __name__=='__main__':
   study = optuna.create_study()
-  study.optimize(objective, n_trials=100)
+  study.optimize(objective, n_trials=10)
 
   pruned_trials = [t for t in study.trials if t.state == optuna.trial.TrialState.PRUNED]
   complete_trials = [t for t in study.trials if t.state == optuna.trial.TrialState.COMPLETE]
